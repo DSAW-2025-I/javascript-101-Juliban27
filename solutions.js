a3deb914eb0e3de060ad409996f282384cfdc66e
Julian Romero <juliban27@gmail.com>
2025-02-17 13:49:19 -0500

    Solucion parte 3

--- solutions.js
+++ solutions.js
@@ -1,10 +1,16 @@
 // Sum of Two Numbers
 function sum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "Error: Input must be numbers";
+  }
   return a + b;
 }
 
 // Factorial of a Number
 function factorial(n) {
+  if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) {
+    return "Error: Input must be a non-negative integer";
+  }
   if (n == 1 || n==0 ) {
     return 1
   }else{
@@ -14,6 +20,9 @@ function factorial(n) {
 
 // Find the Largest Number
 function findLargest(arr) {
+  if (!Array.isArray(arr) || arr.some(x => typeof x !== "number")) {
+    return "Error: Input must be an array of numbers";
+  }
   let Mayor = arr[0];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > Mayor) {
@@ -25,6 +34,9 @@ function findLargest(arr) {
 
 
 function countVowels(str) {
+  if (typeof str !== "string") {
+    return "Error: Input must be a string";
+  }
   let count = 0;
   let strLow = str.toLowerCase();
   for (let i = 0; i < str.length; i++) {
@@ -45,6 +57,9 @@ function countVowels(str) {
 
 // Check if a Number is Prime
 function isPrime(n) {
+  if (typeof n !== "number" || !Number.isInteger(n)) {
+    return "Error: Input must be an integer";
+  }
   if ( n <= 1 ) {
     return false
   }else if (n == 2  || n == 3) {
