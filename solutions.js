52708829f33eabaccfc420bf089cf354603bbdd9
Julian Romero <juliban27@gmail.com>
2025-02-17 13:25:46 -0500

    Respuesta del Ejercicio

--- solutions.js
+++ solutions.js
@@ -1,27 +1,64 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  return a + b;
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  if (n == 1 || n==0 ) {
+    return 1
+  }else{
+    return n = factorial(n-1) * n
+  }
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  let Mayor = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i-1]) {
+      Mayor = arr[i]
+    }
+  }
+  return Mayor
 }
 
-// Count Vowels in a String
+
 function countVowels(str) {
-  // Add your code here
+  let count = 0;
+  let strLow = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (strLow[i] == "a") {
+      count++;
+    } else if (strLow[i] == "e") {
+      count++;
+    } else if (strLow[i] == "i") {
+      count++;
+    }else if (strLow[i] == "o") {
+      count++;
+    }else if (strLow[i] == "u") {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  if ( n <= 1 ) {
+    return false
+  }else if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }else{
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0){
+          return false;
+        }
+    }
+  } 
+  return true
 }
+
 
 module.exports = {
   sum,
